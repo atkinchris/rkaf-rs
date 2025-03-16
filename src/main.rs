6a8e255021c29b07d21e15c792fefdba5d499b6a
Chris Atkin <github@chrisatkin.co.uk>
2025-03-16 15:25:44 +0000

    Add binrw dependency and basic header parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use binrw::{BinRead, binrw};
+use std::fs::File;
+
+const MAX_MODEL_LEN: usize = 0x22;
+
+#[derive(Debug)]
+#[binrw]
+#[brw(big, magic = b"RKAF")]
+struct UpdateHeader {
+    length: u32,
+    model: [u8; MAX_MODEL_LEN],
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let file_path = "embedded-update.img";
+
+    let mut fp = File::open(file_path)?;
+
+    let rkaf = UpdateHeader::read(&mut fp)?;
+    println!("{:?}", rkaf.model);
+
+    Ok(())
 }
