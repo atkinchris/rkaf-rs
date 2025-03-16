e8760afdc815dc25007f8555cbdc99a380dbbbae
Chris Atkin <github@chrisatkin.co.uk>
2025-03-16 15:49:41 +0000

    Validate file size against header

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::fs::File;
 const MAX_MODEL_LEN: usize = 0x22;
 
 #[derive(Debug, BinRead)]
-#[brw(big, magic = b"RKAF")]
+#[brw(little, magic = b"RKAF")]
 struct UpdateHeader {
     length: u32,
     model: [u8; MAX_MODEL_LEN],
@@ -12,15 +12,25 @@ struct UpdateHeader {
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let file_path = "embedded-update.img";
+    let mut file = File::open(file_path)?;
+    let filesize = file.metadata()?.len();
 
-    let mut fp = File::open(file_path)?;
+    println!("Filesize: {}", filesize);
 
-    let rkaf = UpdateHeader::read(&mut fp)?;
+    let header = UpdateHeader::read(&mut file).expect("Could not parse header from file");
 
-    println!("Filesize: {}", rkaf.length);
+    let expected_filesize = header.length as u64 + 4; // 4 bytes for CRC
+    if expected_filesize != filesize {
+        eprintln!(
+            "File length defined in header ({}) does not match actual file length ({})",
+            header.length, filesize
+        );
+        return Err("File length mismatch".into());
+    }
+
     println!(
         "Model: {}",
-        String::from_utf8(rkaf.model.to_vec()).expect("Model should be valid utf8")
+        String::from_utf8(header.model.to_vec()).expect("Model should be valid utf8")
     );
 
     Ok(())
